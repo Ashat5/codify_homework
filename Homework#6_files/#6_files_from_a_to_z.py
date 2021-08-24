d47a6a7dfc6fa8ad397fd662fd6e8e17e5378053
Ashat5 <88671208+Ashat5@users.noreply.github.com>
2021-08-24 22:10:15 +0600

    Homework#6_files_from_a_to_z.py

---
+++ Homework#6_files/#6_files_from_a_to_z.py
@@ -0,0 +1,22 @@
+#Ashat_Polvanov
+
+# 1. Напишите программу для создания 26 текстовых файлов с именами A.txt, B.txt и так далее до Z.txt, в каждом файле будет его имя, 
+# пример:
+# Файл a.txt - содержит a
+# Файл b.txt - содержит b 
+# 2. Добавьте возможность удаления этих 26 файлов.
+import os
+import sys
+
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for i in alphabet:
+  with open(os.path.join(sys.path[0], '%s.txt' % i), 'w') as file:
+    file.write('%s' % i)
+    file.close()
+
+users_input = input('Вы хотите удалить созданные текстовые файлы? Введите да или нет: ')
+if users_input == 'да':
+  for i in alphabet:
+    os.remove(os.path.join(sys.path[0], '%s.txt' % i))
+    
