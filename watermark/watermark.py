128c000791c16546ba8d25ce93580c7e4ad8ee01
unknown <qwerty19881509@gmail.com>
2021-09-17 18:56:57 +0600

    test_job_watermark

--- watermark/watermark.py
+++ watermark/watermark.py
@@ -10,21 +10,17 @@ processing_files = os.listdir(LOAD_PATH)
 
 
 os.mkdir(BASE_SAVE_PATH)
-for k in processing_files:
-    if k.endswith(IMAGE_EXTENSIONS):
-        img = Image.open(LOAD_PATH + k)
-        kn, kext = os.path.splitext(k)
-        insertion_coordinates = []
-        value = len(kn) * 22.6
+for item in processing_files:
+    if item.endswith(IMAGE_EXTENSIONS):
+        img = Image.open(LOAD_PATH + item)
+        image_name, unnecessary_variable = os.path.splitext(item)
+        value = len(image_name) * 22.6
         new_width = img.width - value
         new_height = img.height - 53
-        insertion_coordinates.append(new_width)
-        insertion_coordinates.append(new_height)
-        insertion_coordinates = tuple(insertion_coordinates)
         font = ImageFont.truetype('verdana.ttf', size=40)
         draw_text = ImageDraw.Draw(img)
-        draw_text.text(insertion_coordinates,'%s' % kn, font=font, fill='yellow')
-        img.save(BASE_SAVE_PATH + '%s.jpg' % kn)
+        draw_text.text((new_width, new_height),'%s' % image_name, font=font, fill='yellow')
+        img.save(BASE_SAVE_PATH + '%s.jpg' % image_name)
         img.close()        
 
 
